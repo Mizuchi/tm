c1479fecaaf5c8644ef37a3b65748d151bdcb358
Tianjiao Yin <ytj000@gmail.com>
2015-02-26 01:41:18 -0800

    uniform

--- tm/tm.hpp
+++ tm/tm.hpp
@@ -3,14 +3,16 @@
 namespace type_mutator {
 namespace detail {
 template <int N> struct Cnt : Cnt<N - 1> { static constexpr int depth = N; };
-template <> struct Cnt<0> {};
+template <> struct Cnt<0> { static constexpr int depth = 0; };
 
 template <class T, int N> struct RetType {
     using type = T;
     static constexpr int depth = N;
 };
 
-template <class T, int N> struct RetValue : RetType<T, N> {
+template <class T, int N> struct RetValue {
+    using type = T;
+    static constexpr int depth = N;
     T value;
     constexpr RetValue(T t) : value(t) {}
 };
@@ -19,46 +21,37 @@ template <int N = 100> struct MaxDepth { static constexpr int depth = N; };
 } // namespace detail
 } // namespace type_mutator
 
-#define TM_VAR(Name, ...)                                                      \
-    using TypeMutatorDetailMaxDepth##Name =                                    \
-        ::type_mutator::detail::MaxDepth<__VA_ARGS__>;                         \
-    static ::type_mutator::detail::RetType<void, 0> __detail_tm_helper_##Name( \
-        ::type_mutator::detail::Cnt<0> n) {                                    \
-        TypeMutatorDetailMaxDepth##Name k;                                     \
-        static_assert(k.depth >= 0, "TypeMutator: depth size must >= 0");      \
-        __detail_tm_helper_##Name(n);                                          \
-        return {};                                                             \
+#define DETAIL_TM_EMPTY
+
+#define DETAIL_TM_DEFINE(Name, Index, Return, Type, Expr)                      \
+    constexpr static ::type_mutator::detail::Return<Type, Index>               \
+        __detail_tm_helper_##Name(::type_mutator::detail::Cnt<Index> n) {      \
+        static_assert(n.depth <= DETAIL_TM_MAX_DEPTH(Name),                    \
+                      "TypeMutator " #Name ": Depth size is too small");       \
+        return true ? ::type_mutator::detail::Return<Type, n.depth>(Expr)      \
+                    : __detail_tm_helper_##Name(n);                            \
     }
 
 #define DETAIL_TM_MAX_DEPTH(Name) TypeMutatorDetailMaxDepth##Name::depth
-#define DETAIL_TM_LAST_FUNCTION_CALL(Name)                                         \
+#define DETAIL_TM_LAST_FUNC_CALL(Name)                                         \
     __detail_tm_helper_##Name(                                                 \
         ::type_mutator::detail::Cnt<DETAIL_TM_MAX_DEPTH(Name)>{})
 
-#define DETAIL_TM_LAST_RESULT(Name) decltype(DETAIL_TM_LAST_FUNCTION_CALL(Name))
+#define DETAIL_TM_LAST_RESULT(Name) decltype(DETAIL_TM_LAST_FUNC_CALL(Name))
 
 #define DETAIL_TM_NEXT_INDEX(Name) DETAIL_TM_LAST_RESULT(Name)::depth + 1
 
+#define TM_VAR(Name, ...)                                                      \
+    using TypeMutatorDetailMaxDepth##Name =                                    \
+        ::type_mutator::detail::MaxDepth<__VA_ARGS__>;                         \
+    DETAIL_TM_DEFINE(Name, 0, RetType, void, DETAIL_TM_EMPTY)
+
 #define TM_GET(Name) DETAIL_TM_LAST_RESULT(Name)::type
 #define TM_SET(Name, Type)                                                     \
-    static ::type_mutator::detail::RetType<Type, DETAIL_TM_NEXT_INDEX(Name)>   \
-        __detail_tm_helper_##Name(                                             \
-            ::type_mutator::detail::Cnt<DETAIL_TM_NEXT_INDEX(Name)> n) {       \
-        static_assert(n.depth <= DETAIL_TM_MAX_DEPTH(Name),                    \
-                      "TypeMutator " #Name ": Depth size is too small");       \
-        return {};                                                             \
-        return __detail_tm_helper_##Name(n);                                   \
-    }
+    DETAIL_TM_DEFINE(Name, DETAIL_TM_NEXT_INDEX(Name), RetType, Type,          \
+                     DETAIL_TM_EMPTY)
 
-#define VM_GET(Name) DETAIL_TM_LAST_FUNCTION_CALL(Name).value
+#define VM_GET(Name) DETAIL_TM_LAST_FUNC_CALL(Name).value
 #define VM_SET(Name, Expr)                                                     \
-    constexpr static ::type_mutator::detail::RetValue<                         \
-        decltype(Expr), DETAIL_TM_NEXT_INDEX(Name)>                            \
-        __detail_tm_helper_##Name(                                             \
-            ::type_mutator::detail::Cnt<DETAIL_TM_NEXT_INDEX(Name)> n) {       \
-        static_assert(n.depth <= DETAIL_TM_MAX_DEPTH(Name),                    \
-                      "TypeMutator " #Name ": Depth size is too small");       \
-        return true ? ::type_mutator::detail::RetValue<decltype(Expr),         \
-                                                       n.depth>{(Expr)}        \
-                    : __detail_tm_helper_##Name(n);                            \
-    }
+    DETAIL_TM_DEFINE(Name, DETAIL_TM_NEXT_INDEX(Name), RetValue,               \
+                     decltype(Expr), Expr)
